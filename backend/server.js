d0df5317d3a47ac4e9f25984d9f4902fa410263c
coderanik <anikdas210605@gmail.com>
2025-06-20 21:42:05 +0530

    fixed some major deployment issue

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,11 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://127.0.0.1:3000',
-      'http://127.0.0.1:3001'
-    ];
+      'http://127.0.0.1:3001',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean);
     
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -54,5 +55,4 @@ app.get('/', (req, res) => {
     res.send('Backend is working');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;

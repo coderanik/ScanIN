476aaed28d9608f18a9fc3af3481abbeb01d319e
coderanik <anikdas210605@gmail.com>
2025-06-20 21:44:23 +0530

    fixed some major deployment issue

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Ignores lint errors on build (especially useful for Vercel)
   },
   // other config options can go here
 };
 
-export default nextConfig;
+export default nextConfig; 
